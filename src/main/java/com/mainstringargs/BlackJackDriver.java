0138c0b9107f6bd42b24f0af3a56d1b5bdf8e970
Mark <mark.l.cloutier@gmail.com>
2018-03-08 16:31:45 -0600

    Adding initial unit tests;  Adding method to calculate hand value given cards

--- src/main/java/com/mainstringargs/BlackJackDriver.java
+++ src/main/java/com/mainstringargs/BlackJackDriver.java
@@ -3,12 +3,22 @@ package com.mainstringargs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.TreeMap;
 
 import com.mainstringargs.domain.Card;
+import com.mainstringargs.domain.Rank;
+import com.mainstringargs.domain.Suit;
 
 public class BlackJackDriver {
 
+	private static TreeMap<Card, Integer> dealtCards = new TreeMap<Card, Integer>();
+	private static TreeMap<Card, Integer> undealtCards = new TreeMap<Card, Integer>();
+
 	public static void main(String[] args) {
+
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		System.out.print("Number of Hands: ");
 		String line = null;
@@ -29,6 +39,12 @@ public class BlackJackDriver {
 
 		int numDecks = Integer.parseInt(line);
 
+		for (Suit suit : Suit.values()) {
+			for (Rank rank : Rank.values()) {
+				undealtCards.put(new Card(rank, suit), numDecks);
+			}
+		}
+
 		System.out.println("Initializing with " + numHands + " hand(s) & " + numDecks + " deck(s)");
 
 		while (true) {
@@ -42,7 +58,7 @@ public class BlackJackDriver {
 
 			Card dealerCard = Card.fromString(line);
 			System.out.println("Dealer Card: " + dealerCard);
-
+			dealtCards.put(dealerCard, null);
 			System.out.print("Enter First Card in Hand: ");
 
 			try {
@@ -53,7 +69,7 @@ public class BlackJackDriver {
 
 			Card firstCardHand = Card.fromString(line);
 			System.out.println("First Card in Hand: " + firstCardHand);
-
+			dealtCards.put(firstCardHand, null);
 			System.out.print("Enter Second Card in Hand: ");
 
 			try {
@@ -63,8 +79,66 @@ public class BlackJackDriver {
 			}
 
 			Card secondCardHand = Card.fromString(line);
+			dealtCards.put(secondCardHand, null);
 			System.out.println("Second Card in Hand: " + secondCardHand);
+
+			findMove(dealerCard, firstCardHand, secondCardHand);
 		}
 
+	}
+
+	private static void findMove(Card dealerCard, Card... handCards) {
+
+		int numCardInstances = undealtCards.get(dealerCard);
+		undealtCards.put(dealerCard, --numCardInstances);
+
+		for (Card handCard : handCards) {
+			numCardInstances = undealtCards.get(handCard);
+			undealtCards.put(handCard, --numCardInstances);
+		}
+
+		List<Integer> handValues = getHandValues(handCards);
+
+	}
+
+	public static List<Integer> getHandValues(Card... handCards) {
+
+		// int defaultValue = Arrays.stream(handCards).map(x ->
+		// x.getRank().getDefaultValue())
+		// .reduce((x, y) -> x+y).get();
+
+		List<Integer> runningTotals = new ArrayList<Integer>();
+
+		for (Card card : handCards) {
+
+			if (runningTotals.size() == 0) {
+				runningTotals.add(card.getRank().getDefaultValue());
+
+				if (card.getRank() == Rank.ACE) {
+					runningTotals.add(11);
+				}
+			} else {
+				// snapshot for ACE
+				List<Integer> runningTotalsSnapShot = new ArrayList<Integer>(runningTotals);
+
+				for (int i = 0; i < runningTotals.size(); i++) {
+					int val = runningTotals.get(i);
+					val += card.getRank().getDefaultValue();
+					runningTotals.set(i, val);
+				}
+
+				if (card.getRank() == Rank.ACE) {
+					for (int i = 0; i < runningTotalsSnapShot.size(); i++) {
+						int val = runningTotalsSnapShot.get(i);
+						val += 11;
+						runningTotals.add(val);
+					}
+				}
+			}
+
+		}
+		Collections.sort(runningTotals);
+
+		return runningTotals;
 	}
 }
--- src/test/java/com/mainstringargs/BlackJackDriverTest.java
+++ src/test/java/com/mainstringargs/BlackJackDriverTest.java
@@ -1,5 +1,9 @@
+package com.mainstringargs;
+
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
@@ -25,12 +29,71 @@ public class BlackJackDriverTest {
 	}
 
 	@Test
-	public void testGetHandValuesNoneAce() {
+	public void testGetHandValuesNoAce() {
 		for (Card card : allCards) {
 			if (card.getRank() != Rank.ACE) {
-				assertEquals(card.getRank().getDefaultValue(), BlackJackDriver.getHandValues(card));
+				assertEquals(card.getRank().getDefaultValue(), BlackJackDriver.getHandValues(card).get(0).intValue());
 			}
 		}
+	}
+
+	@Test
+	public void testGetHandValuesAce() {
+
+		Card ace = new Card(Rank.ACE, Suit.CLUBS);
+		assertEquals(Arrays.asList(1, 11), BlackJackDriver.getHandValues(ace));
+
+	}
+
+	@Test
+	public void testGetHandValuesTwoCardsNoAce() {
+		for (Card cardA : allCards) {
+			for (Card cardB : allCards) {
+
+				if (cardA.getRank() != Rank.ACE && cardB.getRank() != Rank.ACE) {
+
+					int calculatedTotal = cardA.getRank().getDefaultValue() + cardB.getRank().getDefaultValue();
+
+					assertEquals(calculatedTotal, (int) BlackJackDriver.getHandValues(cardA, cardB).get(0));
+				}
+
+			}
+		}
+	}
+
+	@Test
+	public void testGetHandValuesThreeCardsNoAce() {
+		for (Card cardA : allCards) {
+			for (Card cardB : allCards) {
+				for (Card cardC : allCards) {
+					if (cardA.getRank() != Rank.ACE && cardB.getRank() != Rank.ACE && cardC.getRank() != Rank.ACE) {
+
+						int calculatedTotal = cardA.getRank().getDefaultValue() + cardB.getRank().getDefaultValue()
+								+ cardC.getRank().getDefaultValue();
+
+						assertEquals(calculatedTotal, (int) BlackJackDriver.getHandValues(cardA, cardB, cardC).get(0));
+					}
+				}
+			}
+		}
+	}
+
+	@Test
+	public void testGetHandValuesTwoCardsTwoAces() {
+		Card cardA = new Card(Rank.ACE, Suit.CLUBS);
+		Card cardB = new Card(Rank.ACE, Suit.HEARTS);
+		assertEquals(Arrays.asList(2, 12, 12, 22), BlackJackDriver.getHandValues(cardA, cardB));
+
+	}
+
+	@Test
+	public void testGetHandValuesTwoCardsThreeAces() {
+		Card cardA = new Card(Rank.ACE, Suit.CLUBS);
+		Card cardB = new Card(Rank.ACE, Suit.HEARTS);
+		Card cardC = new Card(Rank.ACE, Suit.SPADES);
+		List<Integer> expected = Arrays.asList(3, 13, 13, 13, 23, 23, 23, 33);
+		List<Integer> actual = BlackJackDriver.getHandValues(cardA, cardB, cardC);
+		assertTrue(expected.size() == actual.size() && expected.containsAll(actual) && actual.containsAll(expected));
 	}
 
 }
